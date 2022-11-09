7249f85998246fa6b447e0ffab8ee7a89b5f3f3b
mariasantizo <maria.santizo@opendeusto.es>
2022-11-09 12:24:29 +0100

    models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    imagen = models.ImageField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nombre
+
+class Ordenador(Producto):
+    #Atributos
+
+    def __str__(self):
+        return self.nombre
+
+class Telefono(Producto):
+    #Atributos
+
+    def __str__(self):
+        return self.nombre
+
+class Audio(Producto):
+    #Atributos
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    usuario = models.CharField(max_length=60)
+    contrasenya = models.CharField(max_length=60)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    productos_guardados = models.ManyToManyField(Producto)
