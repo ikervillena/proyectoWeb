7080034b44c61e1a3a03e8efefcb0507e955ff4c
Malen Sanz <malensanz@opendeusto.es>
2022-11-23 18:53:50 +0100

    login

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "inicio/"
