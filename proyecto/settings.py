18f6435ecbf9149ef6706ae0f04fed000acce05c
Malen Sanz <malensanz@opendeusto.es>
2022-12-09 17:24:06 +0100

    nuevo estilo admin

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polymorphic',
     'myapp',
+    'colorfield',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
