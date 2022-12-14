d7998c43d422f2d2d84b5a6c2dda33a0f7573442
Malen Sanz <malensanz@opendeusto.es>
2022-12-14 16:48:32 +0100

    efectos js

--- myapp/static/js/detalle.js
+++ myapp/static/js/detalle.js
@@ -26,3 +26,34 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+//Efectos carrito y favoritos
+const mover = {
+  transform: 'translateY(-100em)'
+}
+
+const timing = {
+  duration: 2000,
+  iteration: 1,
+}
+
+//Carrito
+var a = document.getElementById('carrito');
+a.addEventListener("click", carrito)
+
+function carrito(){
+  document.getElementById('imgcarrito').style.width= '2.5%';
+  document.getElementById('imgcarrito').style.height= '4%';
+  document.getElementById('imgcarrito').animate(mover,timing);
+}
+
+//Favorito
+var b = document.getElementById('favorito');
+b.addEventListener("click", fav)
+
+function fav(){
+  document.getElementById('imgfav').style.width= '2.5%';
+  document.getElementById('imgfav').style.height= '4%';
+  document.getElementById('imgfav').animate(mover,timing);
+}
